50e114c86f9970b88dcb55908533e697b5fbecd1
Spencer Thompson <st746866@southeast.edu>
2023-02-08 15:03:06 -0500

    Lease parent class

---
+++ Models/Lease.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCC_Marina.Models
+{
+    public class Lease
+    {
+        public int LeaseId { get; set; }
+        public int BoatId { get; set; }
+        public int CustomerId { get; set; }
+        public int SlipId { get; set; }
+        public float Rate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
