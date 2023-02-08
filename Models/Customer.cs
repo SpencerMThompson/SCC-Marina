aadb57b866203f38f75607aff541c29e98548662
Collyn-Brummett <cb717599@southeast.edu>
2023-02-08 13:47:37 -0600

    First Commit / add Customer Class

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCC_Marina.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address{ get; set; }
+        public string Phone { get; set; }
+    }
+}
