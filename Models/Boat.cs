4fa48b288283827e78fc51a0ad7d9a1b68a0d16f
Zane Morgan <zm469692@southeast.edu>
2023-02-08 13:54:22 -0600

    Commit for boat class

---
+++ Models/Boat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCC_Marina.Models
+{
+    public class Boat
+    {
+        public int BoatID { get; set; }
+        public string Registration { get; set; }
+        public string BoatLength { get; set; }
+        public string Manufacturer { get; set; }
+        public string Year { get; set; }
+    }
+}
