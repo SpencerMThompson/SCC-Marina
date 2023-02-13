c9a50ffc3fc4e916bedc5c7df20ef54de29afb7f
Zane Morgan <zm469692@southeast.edu>
2023-02-13 13:22:43 -0600

    Added slip class

---
+++ Models/Slip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCC_Marina.Models
+{
+    public class Slip
+    {
+        public int DockID { get; set; }
+
+        public int SlipID { get; set; }
+
+        public string Width { get; set; }
+
+        public string SlipLength { get; set; }
+
+        public Slip()
+        {
+
+        }
+
+        public Slip(int dockID, int slipID, string width, string slipLength)
+        {
+            DockID = dockID;
+            SlipID = slipID;
+            Width = width;
+            SlipLength = slipLength;
+        }
+    }
+}
