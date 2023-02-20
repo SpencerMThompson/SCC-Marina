04af28a1d28e1062d31400523697a6f18cd2aa38
Collyn Brummett <cb717599@southeast.edu>
2023-02-20 13:37:50 -0600

    Updating code

---
+++ Migrations/202302201908402_updatingDB.cs
@@ -0,0 +1,46 @@
+namespace SCC_Marina.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatingDB : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Boats", "BoatType", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.Boats", "Registration", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.Boats", "BoatLength", c => c.String(nullable: false, maxLength: 10));
+            AlterColumn("dbo.Boats", "Manufacturer", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.Boats", "Year", c => c.String(nullable: false, maxLength: 4));
+            AlterColumn("dbo.Boats", "NumberOfEngines", c => c.String(maxLength: 10));
+            AlterColumn("dbo.Boats", "FuelType", c => c.String(maxLength: 25));
+            AlterColumn("dbo.Boats", "KeelDepth", c => c.String(maxLength: 10));
+            AlterColumn("dbo.Boats", "NumberOfSails", c => c.String(maxLength: 10));
+            AlterColumn("dbo.Boats", "MotorType", c => c.String(maxLength: 25));
+            AlterColumn("dbo.Customers", "FName", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.Customers", "LName", c => c.String(nullable: false, maxLength: 25));
+            AlterColumn("dbo.Customers", "Address", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.Customers", "Phone", c => c.String(nullable: false, maxLength: 11));
+            AlterColumn("dbo.Leases", "LeaseType", c => c.String(nullable: false, maxLength: 25));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Leases", "LeaseType", c => c.String());
+            AlterColumn("dbo.Customers", "Phone", c => c.String());
+            AlterColumn("dbo.Customers", "Address", c => c.String());
+            AlterColumn("dbo.Customers", "LName", c => c.String());
+            AlterColumn("dbo.Customers", "FName", c => c.String());
+            AlterColumn("dbo.Boats", "MotorType", c => c.String());
+            AlterColumn("dbo.Boats", "NumberOfSails", c => c.String());
+            AlterColumn("dbo.Boats", "KeelDepth", c => c.String());
+            AlterColumn("dbo.Boats", "FuelType", c => c.String());
+            AlterColumn("dbo.Boats", "NumberOfEngines", c => c.String());
+            AlterColumn("dbo.Boats", "Year", c => c.String());
+            AlterColumn("dbo.Boats", "Manufacturer", c => c.String());
+            AlterColumn("dbo.Boats", "BoatLength", c => c.String());
+            AlterColumn("dbo.Boats", "Registration", c => c.String());
+            AlterColumn("dbo.Boats", "BoatType", c => c.String());
+        }
+    }
+}
